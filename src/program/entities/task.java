457ca39996ed2ca478fdf900d0bc68d5fac7c541
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-28 19:43:23 -0300

    task

--- src/program/entities/task.java
+++ src/program/entities/task.java
@@ -8,7 +8,38 @@ public class task {
     private String description;
     private Date date;
 
+    public task() {
+    }
 
+    public task(String title, String description, Date date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
 
 }
