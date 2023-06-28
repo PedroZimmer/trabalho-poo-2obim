a2abebc265c2ca48b354a4633c2b0aff41f96011
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-28 19:49:57 -0300

    pendingTask

--- src/program/entities/pendingTask.java
+++ src/program/entities/pendingTask.java
@@ -1,4 +1,40 @@
 package program.entities;
 
-public class pendingTask {
+import java.util.Date;
+import java.util.HashMap;
+
+public class pendingTask extends task{
+
+
+    public pendingTask() {
+    }
+
+    pendingTask(String title, String description, Date date) {
+        super(title, description, date);
+    }
+
+    private HashMap<Integer,String> listPendingTask = new HashMap<>();
+
+    public void addPendingTask(int id, String title){
+        listPendingTask.put(id, title);
+    }
+
+    public void printPendingTask(){
+        for (Integer key : listPendingTask.keySet()) {
+            System.out.println(key + " - " + listPendingTask.get(key));
+        }
+    }
+
+    public void removePendingTask(int id){
+        listPendingTask.remove(id);
+    }
+
+    public void clearPendingTask(){
+        listPendingTask.clear();
+    }
+
+
+
+
+
 }
